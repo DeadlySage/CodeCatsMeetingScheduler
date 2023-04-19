e82998d35754f6abf6a58d0e6ffe3397fb74c3d0
Vegeta <87342072+Vegeta58@users.noreply.github.com>
2023-04-19 12:51:50 -0700

    Add files via upload
    redirected the Instructor/Lab Advisor to the Appointment Selection Page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,35 +1,50 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-
-import { IconContext } from "react-icons";
-import { FaSchool } from "react-icons/fa"
-import { MdSchool } from "react-icons/md"
+import { IconContext } from 'react-icons';
+import { FaSchool } from 'react-icons/fa';
+import { MdSchool } from 'react-icons/md';
 
 const Home = () => {
+  return (
+    <IconContext.Provider value={{ size: '4em' }}>
+      <div
+        className="auth-form-container"
+        style={{
+          display: 'flex',
+          justifyContent: 'top',
+          padding: '30px 30px',
+        }}
+      >
+        <h2>Are you a student or an instructor?</h2>
+        <div
+          className="button-container"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            gap: '10px',
+            flexDirection: 'row',
+            alignItems: 'flex-start',
+          }}
+        >
+          <button className="button" style={{ width: '200px' }}>
+            <p>
+              <MdSchool />
+            </p>
+            <span style={{ fontSize: 18, fontWeight: 'bold' }}>Student</span>
+          </button>
 
-    return (
-        <IconContext.Provider value = {{size: "4em"}}>
-        <div className="auth-form-container"
-            style={{ display: "flex", justifyContent: "top", padding: "30px 30px" }}
-            >
-            <h2>Are you a student or an instructor?</h2>
-            <div className="button-container"
-                style={{ display: "flex", justifyContent: "center", gap: "10px", flexDirection: "row", alignItems: "flex-start" }}
-            >      
-                <button className="button" style={{ width: "200px" }}>
-                 <p><MdSchool /> 
-                 </p> 
-                 <span style={{fontSize: 18, fontWeight: 'bold'}} >Student </span>
-                </button>
-               
-                <button className="button" style={{ width: "200px" }}>
-                 <p><FaSchool /> 
-                 </p>     
-                 <span style={{fontSize: 18, fontWeight: 'bold'}} >Instructor </span>
-                </button>
-            </div>    
+          <Link to="/appointment-selection">
+            <button className="button" style={{ width: '200px' }}>
+              <p>
+                <FaSchool />
+              </p>
+              <span style={{ fontSize: 18, fontWeight: 'bold' }}>Instructor</span>
+            </button>
+          </Link>
         </div>
-        </IconContext.Provider>
-    )
-}
+      </div>
+    </IconContext.Provider>
+  );
+};
 
 export default Home;
