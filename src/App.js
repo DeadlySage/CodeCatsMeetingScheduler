71cfd104633876519f8ff12ecb3d9fb202ac2ef1
iubs <99505642+iubs@users.noreply.github.com>
2023-09-17 21:34:31 -0700

    Refactor the front end as to load home automatically.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import SpongeBob_Squarepants from "./Components/advisor_calendar/SpongeBob_Squar
 import Patrick_Star from "./Components/advisor_calendar/Patrick_Star";
 import Yu_Chen from "./Components/advisor_calendar/Yu_Chen";
 import Navbar from "./Components/Navbar";
+import { Navigate } from 'react-router-dom';
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -26,22 +27,23 @@ function App() {
     <div className="main">
       <Router>
         <div className="topnav">
-          <Navbar/>
+          <Navbar />
         </div>
         <div className="App">
           <Routes>
             <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Navigate replace to="/home" />} />
             <Route path="/login" element={<Login onFormSwitch={() => setCurrentForm("register")} />} />
             <Route path="/register" element={<Register onFormSwitch={() => setCurrentForm("login")} />} />
             <Route path="/student-selection-course" element={<StudentSelectionCourse />} />
             <Route path="/appointment-selection" element={<AppointmentSelection />} />
             <Route path="/advisor-selection" element={<AdvisorSelection />} />
             <Route path="/password-reset" element={<PasswordReset />} />
-            <Route path="/calendar" element ={<Calendar />} />
-            <Route path="/Kenneth_Elliot" element ={<Kenneth_Elliot />} />
-            <Route path="/SpongeBob_Squarepants" element ={<SpongeBob_Squarepants />} />
-            <Route path="/Yu_Chen" element ={<Yu_Chen/>} />
-            <Route path="/Patrick_Star" element ={<Patrick_Star />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/Kenneth_Elliot" element={<Kenneth_Elliot />} />
+            <Route path="/SpongeBob_Squarepants" element={<SpongeBob_Squarepants />} />
+            <Route path="/Yu_Chen" element={<Yu_Chen />} />
+            <Route path="/Patrick_Star" element={<Patrick_Star />} />
           </Routes>
         </div>
       </Router>
