82b76c71837284167d8206a40ecca802bf968c82
lamphancs <121737038+lamphancs@users.noreply.github.com>
2023-09-17 20:23:18 -0700

    connect advisor list with backend database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const uri = "mongodb+srv://Admin:4321admin@cluster0.lyj1ljm.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://lphan:OqdpVS85l4ivEoQ2@cluster0.lyj1ljm.mongodb.net/Course?retryWrites=true&w=majority"
+
+const courseSchema = new mongoose.Schema({
+  courseID: String,
+  instructor: [String]
+});
+
+const Course = mongoose.model("Course", courseSchema);
 
 async function connect(){
     try{
@@ -12,6 +19,22 @@ async function connect(){
     }
 
 }
+
+app.get("/api/course", async (req, res) => {
+  try {
+    // Add a new course
+    // const newCourse = new Course({ courseID: "test", instructor: ["test"] });
+    // await newCourse.save();
+
+    // Fetch all courses
+    const course = await Course.find();
+    console.log("Courses:", course);
+    res.json(course);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch course" });
+  }
+});
 
 connect();
 app.listen(3000, () => {
