a5a09878be242132bcc6fa13a72d1fbcbd693137
lawz <93344890+lawz4@users.noreply.github.com>
2023-10-01 23:39:18 -0700

    Update AdvisorSelection.jsx
    NOTE:
    code builds and runs
    
    need to do more work on users part of database and how it handles so we can effectively retrieve users from DB that has the appropriate roles

--- src/Components/AdvisorSelection.jsx
+++ src/Components/AdvisorSelection.jsx
@@ -1,34 +1,68 @@
-import React, { useState, useEffect  } from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-
 const AdvisorSelection = () => {
     const navigate = useNavigate();
+    
+
+    /* 
+    Michael Lawler - 10.1.23
+    
+    GOAL:
+    // advisor selection with list buttons with instructors in database
+    
+    PREVIEW
+    dynamically populate view with a button for each advisor
+    that is returned in the get request w/ the button label
+    being the user's first and last name
+
+    NOTE:
+    code builds and runs
+    need to do more work on users part of database and how it handles
+    so we can effectively retrieve users from DB that has the
+    appropriate roles
+
+    START
+    */
+    
+    const [advisors, setAdvisors] = useState([]);
+    
+    // get request to fetch users with role of instructor, admin
+    // adjust endpoint url that returns desired users
+    useEffect(() => {
+        fetch("/api/users")
+           .then((response) => {
+             if (!response.ok) {
+               throw new Error("Network response was not ok");
+             }
+             return response.json();
+           })
+           .then((data) => {
+                // filter users by role and format name
+                const instructorAdminUsers = data.filter(
+                    user => user.role_id === 'instructor' || user.role_id === 'admin'
+                ).map(user => ({
+                    // format name as "Last, First"
+                    name: `${user.last_name}, ${user.first_name}`,
+                    // create a route for each user
+                    route: `/${user.last_name}_${user.first_name}`
+                }));
+                setAdvisors(instructorAdminUsers);
+           })
+           .catch((error) => {
+                console.error("Fetch error:", error);
+           });
+       }, []);
+
     const handleAdvisorClick = (advisorName) => {
         navigate(`/advisor-calendar/${advisorName}`);
     }
 
-    /* still working on showing data from "/api/course" to UI */
-    // const [advisors, setAdvisors] = useState([]);
-    // useEffect(() => {
-    //     // Fetch advisors when the component mounts
-    //     fetch("/api/course")
-    //       .then((response) => {
-    //         if (!response.ok) {
-    //           throw new Error("Network response was not ok");
-    //         }
-    //         console.log(response);
-    //         return response.json();
-    //       })
-    //       .then((data) => setAdvisors(data))
-    //       .catch((error) => {
-    //         console.error("Fetch error:", error);
-    //         // Handle the error, e.g., display an error message to the user
-    //       });
-    //   }, []);
+    // END of Michael Lawler - 10.1.23
     /************************************************************/
-      
+    
     return (
         <div className="auth-form-container"
             style={{ display: "flex", justifyContent: "top", padding: " 25px 25px 50px 25px" }}
